eaee90123a817440e19181068b0c852c69b10e2b
jianxun <jianxunbak@gmail.com>
2025-03-10 22:26:46 +0800

    added render api url

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080";
+// const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://spring-boot-back-end-project.onrender.com";
 
 const Api = axios.create({
   baseURL: BASE_URL,
