4e839f215e1a73715ae9327841e4fdf1f1ff1594
jianxun <jianxunbak@gmail.com>
2025-03-10 22:37:48 +0800

    port

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: import.meta.env.VITE_PORT || 3000, // Fallback to 3000 for local development
+  },
+});
